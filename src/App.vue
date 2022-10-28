66383e2888976aae23c188dd154d4d069aa47cd0
Dan Beland <dan@danbeland.com>
2022-10-27 22:18:56 -0700

    Set up basic time tracker

--- src/App.vue
+++ src/App.vue
@@ -1,27 +1,98 @@
 <template>
   <div class="container pt-5" style="max-width: 768px;">
     
-    <button class="btn btn-primary float-end"><i class="fa-solid fa-plus"></i></button>
+    <button class="btn btn-primary float-end" @click="addTask()"><i class="fa-solid fa-plus"></i></button>
     <h1 class="text-center mb-5">Time Tracker</h1>
     
-    <TaskList tasks="{{ tasks }}" />
+    <TaskItem 
+      v-for="task in tasks" 
+      :key="task.id" 
+      :task="task" 
+      v-model="task.name"
+      @toggle="toggleTask"
+      @reset="resetTask"
+      @remove="removeTask"
+      />
 
-    <div class="mt-5 text-center text-muted"><small>&copy; 2022 Choss Labs.</small></div>
+    <div class="mt-5 text-center text-muted"><small>&copy; {{ new Date().getFullYear() }} Choss Labs.</small></div>
   </div>
 </template>
 
 <script>
-import TaskList from './components/TaskList.vue';
+import TaskItem from './components/TaskItem.vue';
+
+let taskid = 1;
 
 export default {
   name: 'App',
   data() {
     return {
-      tasks: []
+      tasks: [
+        { 
+          id: taskid,
+          name: '',
+          running: false,
+          interval: null,
+          duration: 0
+        }
+      ] 
+    }
+  },
+  methods: {
+    addTask() {
+      taskid++;
+      this.tasks.push(
+        { 
+          id: taskid,
+          name: '',
+          running: false,
+          interval: null,
+          duration: 0
+        }
+      );
+    },
+    toggleTask(task) {
+      if(task.running) {
+        this.stopTask(task);
+      } else {
+        this.startTask(task);
+      }
+    },
+    startTask(task) {
+      task.running = true;
+      task.interval = setInterval(() => { 
+        this.incrementTask(task);
+      }, 1000);
+      // toast the other tasks
+      for(let t of this.tasks) {
+        if(t.id != task.id) {
+          this.stopTask(t);
+        }
+      }
+      console.log(task);
+    },
+    stopTask(task) {
+      task.running = false;
+      if(task.interval != null) clearInterval(task.interval);
+    },
+    incrementTask(task) {
+      task.duration++;
+    },
+    resetTask(task) {
+      if(confirm('Are you sure?')) task.duration=0;
+    },
+    removeTask(task) {
+      if(confirm('Are you sure?')) {
+        let index = this.tasks.indexOf(task);
+        if(index != -1) {
+          this.stopTask(this.tasks[index]);
+          this.tasks.splice(index ,1);
+        }
+      }
     }
   },
   components: {
-    TaskList
+    TaskItem
   }
 }
 </script>
--- src/components/TaskItem.vue
+++ src/components/TaskItem.vue
@@ -1,14 +1,12 @@
 <template>
-  <div class="taskItem">
-    <div class="row">
-      <div class="col-7">
-        <input type="text" class="form-control" placeholder="Task Name">
-      </div>
-      <div class="col-3 d-flex justify-content-center align-items-center h4 mb-0">00:00:00</div>
-      <div class="col-2 d-flex justify-content-end">
-        <button class="btn text-success"><i class="fa-solid fa-play"></i></button>
-        <button class="btn text-secondary"><i class="fa-solid fa-rotate"></i></button>
-        <button class="btn text-muted"><i class="fa-solid fa-trash"></i></button>
+  <div class="taskItem mb-3">
+    <div class="d-flex">
+      <div class="name"><input type="text" v-model="inputValue" class="form-control" placeholder="Task Name"></div>
+      <div class="duration h4 mb-0">{{ durationLabel }}</div>
+      <div class="actions">
+        <button class="btn" @click="this.$emit('toggle', this.task)" tabindex="-1"><i class="fa-solid" :class="{ 'text-success fa-play': !task.running, 'text-warning fa-pause': task.running }"></i></button>
+        <button class="btn text-secondary" @click="this.$emit('reset', this.task)" tabindex="-1"><i class="fa-solid fa-rotate"></i></button>
+        <button class="btn text-muted" @click="this.$emit('remove', this.task)" tabindex="-1"><i class="fa-solid fa-trash"></i></button>
       </div>
     </div>
   </div>
@@ -16,11 +14,40 @@
 
 <script>
 export default {
-  name: 'TaskItem'
+  name: 'TaskItem',
+  props: {
+    task: Object,
+    modelValue: String
+  },
+  computed: {
+    inputValue: {
+      get() {
+        return this.task.name;
+      },
+      set(value) {
+        this.$emit('update:modelValue', value);
+      }
+    },
+    durationLabel() {
+      let seconds = Math.floor((this.task.duration) % 60),
+      minutes = Math.floor((this.task.duration / 60) % 60),
+      hours = Math.floor((this.task.duration / (60 * 60)) % 24);
+      hours = (hours < 10) ? "0" + hours : hours;
+      minutes = (minutes < 10) ? "0" + minutes : minutes;
+      seconds = (seconds < 10) ? "0" + seconds : seconds;
+      return hours + ":" + minutes + ":" + seconds;
+    }
+  }
 }
 </script>
 
 <style scoped>
-.taskItem { background-color: white; border-radius: 50px; padding: 5px; }
-.taskItem input[type=text] { border-width: 0; border-radius: 50px; box-shadow: none; }
+.taskItem { background-color: white; border-radius: 50px; padding: 5px; transition: all 200ms ease; box-shadow: 0px 5px 10px -5px rgb(0 0 0 / 10%); } 
+.taskItem .name { flex-grow: 1; }
+.taskItem .duration { flex-shrink: 1; display: grid; place-items: center; padding: 0 5px; }
+.taskItem .actions { flex-shrink: 1; padding: 0 15px; white-space: nowrap; }
+.taskItem .actions button { padding: 5px 7px; }
+.taskItem input[type=text] { width: 100%; border-width: 0; border-radius: 50px; box-shadow: none; font-weight: 500; }
+.taskItem:hover { box-shadow: 0px 5px 10px -5px rgb(0 0 0 / 30%); }
+.fa-trash:hover { color: var(--bs-danger); }
 </style>
